2aa8b1c87aaad20abb0fe8b6ee0177bf4f02d8f6
Jeet <jeet@springboard.com>
2017-03-12 11:56:07 +0530

    Added another basic example with one JS file included.

---
+++ example2/input.js
@@ -0,0 +1,4 @@
+require('./second.js');
+var i = 0;
+console.log('Hello Webpack!');
+console.log('Webpack is cool.');
---
+++ example2/webpack.config.js
@@ -0,0 +1,8 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './input.js',
+  output: {
+    filename: 'output.js'
+  }
+}
