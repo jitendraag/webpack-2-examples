a161afbe2deb2a83c4145a73307df71945a0bd60
Jeet <jeet@springboard.com>
2017-03-13 07:28:48 +0530

    Added resolve and uglify plugins.

---
+++ example13/input.js
@@ -0,0 +1,8 @@
+require('./style.css');
+var second = require('./second');
+
+var i = 0;
+console.log('Hello Webpack!');
+console.log('Webpack is cool.');
+second.welcomeWebpack();
+console.log(second.k);
