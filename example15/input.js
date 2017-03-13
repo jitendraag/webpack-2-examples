721ce3d5699923e124fc1d641ca16b343985ebdb
Jeet <jeet@springboard.com>
2017-03-13 08:27:35 +0530

    Added ES6 classes and babel loader.

---
+++ example15/input.js
@@ -0,0 +1,8 @@
+import './style.scss';
+import { WelcomeWebpack } from './second';
+
+var i = 0;
+console.log('Hello Webpack!');
+console.log('Webpack is cool.');
+w = new WelcomeWebpack();
+w.welcomeWebpack();
---
+++ example15/second.js
@@ -0,0 +1,8 @@
+export var j = 'hello';
+export const k = "ignore me";
+export class WelcomeWebpack {
+  welcomeWebpack() {
+    console.log("Welcome webpack!");
+  }
+}
+console.log("Printing from second.js");
